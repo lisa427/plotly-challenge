a5f0964d4e437843ccbdc3f0cce37890dd7ee847
lisa427 <lisa.mcgowan4@gmail.com>
2021-01-03 21:12:33 -0500

    created bubble plot

--- static/js/app.js
+++ static/js/app.js
@@ -2,24 +2,45 @@ let id = "940";
 //let names = data.names;
 
 
-function buildBarPlot(id) {
+function buildPlots(id) {
       
     d3.json("samples.json").then(function(data) {
 
         let samplesArray = data.samples
         let filteredData = samplesArray.filter(d => d.id === id);
-  
         let sampleValues = filteredData[0].sample_values;
         let otuIds = filteredData[0].otu_ids;
-        let otuLables = filteredData[0].otu_labels;
+        let otuLabels = filteredData[0].otu_labels;
       
         console.log(data);
         console.log(filteredData);
         console.log(sampleValues);
         console.log(otuIds);
-        console.log(otuLables);
-    
+        console.log(otuLabels);
+
+        var trace1 = {
+            x: otuIds,
+            y: sampleValues,
+            text: otuLabels,
+            mode: 'markers',
+            marker: {
+              color: otuIds,
+              size: sampleValues
+            }
+          };
+          
+          var data = [trace1];
+          
+          var layout = {
+            xaxis: {title: 'OTU ID'},
+            showlegend: false,
+            height: 600,
+            width: 1200
+          };
+          
+          Plotly.newPlot('bubble', data, layout);
+        
     });
 };
 
-buildBarPlot(id);
+buildPlots(id);
